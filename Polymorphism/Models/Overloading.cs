9e7d54f538efe42d01e6b50c3c273d8bacd360b3
deebify <yassine.deeb@msn.com>
2016-11-30 20:29:05 +0200

    Project Files Cleaned

---
+++ Polymorphism/Models/Overloading.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace polymorphism // Poly-"Multiple"-Morph-"Forms"
+//Polymorphism -- Multiple Shapes/Forms 
+{
+	//Static Polymorphism
+		// Function Overloading / Operator Overloading
+	//Dynamic Polymorephism 
+		// 1- abstract classes 
+		// 2- virtual functions
+
+	// Just Like Interfaces.
+	// One Interface 1 -- n Many Function  
+	public class Overloading
+	{
+		//Overloading -- Compile Time -- Static Binding -- Early Binding
+		// Different Functionality of Method with "Same Name"
+		// But Different Parameter List. -- All in Same Class 
+		// same return time -- which one if just the return type
+
+
+		//Override -- Runtime Time -- Dynamic Binding -- Late Binding
+		// Same Name and Same Parameter List in different Class. 
+
+
+		//different signatures -- return type not part of method signature "Name+ParameterList".
+		//return type not part of signature. 
+
+		// By depending on the parameters provided for the method, in the run time, compiler determines which version of the method to execute.
+
+		//Overloading (not really polymorphism) is 
+		// simply multiple functions which have the same name 
+		// but different signatures 
+		// (think multiple constructors for an object taking different numbers of arguments). 
+		// Which method is called is known at compile time, 
+		// --> because the arguments are specified at this time.
+
+		public Overloading ()
+		{
+			Console.WriteLine ("This is Defualt Constructor -- NO ARGs");
+		}
+
+		public Overloading (int item)
+		{
+			Console.WriteLine ("This is Constructor -- with "+item);
+		}
+
+		public Overloading (int a,int b)
+		{
+			Console.WriteLine ("This is Constructore -- "+ a +" "+ b);
+		}
+
+		public void Show(){
+			Console.WriteLine ("Hello No Args.");
+		}
+
+		public void Show(int item){
+			Console.WriteLine ("Hello int "+item);
+		}
+
+		public void Show(float item){
+			Console.WriteLine ("Hello float "+item);
+		}
+
+		public void Show(String item){
+			Console.WriteLine ("Hello String "+item);
+		}
+
+		public int ShowX(){
+			Console.WriteLine ("Hello Show");
+			return 1;
+		}
+
+	}
+}
+
---
+++ Polymorphism/Models/Overriding.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using polymorphism;
+
+namespace polymorphism
+{
+	// Just Like a Common Interface
+	// Partial Class Implementation 
+	// You can implement now, or Leave the implementation to Later Classes
+	public abstract class Overriding
+	{
+		// "Dynamic polymorphism" is implemented by 
+			// 1- abstract classes -- no implementation 
+			// 2- virtual functions -- semi-implementation 
+
+		/**
+		 * Overridden functions are functions that have the same signature, 
+		 * but are implemented in different derived classes.
+		 * 
+		 * At "compile time", usually the base class type is used to reference an object, 
+		 * though at run time this object could be of a derived type, 
+		 * so when an overridden method is called, 
+		 * 
+		 * the implementation that is called is 
+		 * dependent on what kind of object is doing the calling 
+		 * (base vs. a derived type) 
+		 * which is unknown at compile time.
+		 * */
+
+		public int x;
+		public int y;
+
+		public void Show(int x){
+			Console.WriteLine ("This is X = "+x);
+		}
+
+		public virtual void ShowX(float X){
+			Console.WriteLine ("This is Semi-implementation in Parent ");
+		}
+		public abstract void Show();
+
+	}
+
+	public class Child : Overriding {
+
+		// this is Local Method to the Child Class 
+		// can not be called by Parent Class. 
+		// But can access the parent (variables/methods/properties).
+		public void GetAndSetX(){
+			Console.WriteLine (this.x);
+			this.x = 100;
+			Console.WriteLine (this.x);
+		}
+
+		public void GetAndSetY(){
+			Console.WriteLine (this.y);
+			this.y = 100;
+			Console.WriteLine (this.y);
+		}
+		//Show(int x) -- Not in Here -- 
+		// but it will call the parent signature. 
+		// you cannot override it unless abstract in parent
+
+		public override void Show ()
+		{
+			Console.WriteLine ("This is Implementation .. ");
+		}
+
+		public override void ShowX (float X)
+		{
+			base.ShowX (X);
+			Console.WriteLine ("This is Final Implementation of ShowX " + X);
+		}
+
+		/** 
+		 * The virtual functions could be implemented differently in different inherited class 
+		 *  and the call to these functions will be decided at runtime.
+		 * 
+		 * */
+
+
+
+
+	}
+}
+
